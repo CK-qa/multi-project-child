6753f0a090a2d8496e8fae7d054d6fec5949f0b8
Viktoria Bozhko <viktoria.bozhko@jetbrains.com>
2019-07-24 17:17:16 +0300

    Initial commit

---
+++ src/main/java/ClassA.java
@@ -0,0 +1,6 @@
+public class ClassA {
+
+    public int a(int a) {
+        return a + 1;
+    }
+}
---
+++ src/test/java/ClassATest.java
@@ -0,0 +1,9 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClassATest {
+
+    @org.junit.jupiter.api.Test
+    void a() {
+        
+    }
+}
